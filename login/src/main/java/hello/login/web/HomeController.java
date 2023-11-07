2e432639cd860ee098e71ffec5f8b98be7706ee2
LeeSPyo <oco141@naver.com>
2023-11-07 14:29:41 +0900

    로그인 처리하기 - 서블릿 HTTP 세션1

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,6 +1,7 @@
 package hello.login.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-	
+
 	private final MemberRepository memberRepository;
 	private final SessionManager sessionManager;
 
@@ -43,7 +44,7 @@ public class HomeController {
 		return "loginHome";
 	}
 
-	@GetMapping("/")
+	// @GetMapping("/")
 	public String homeLoginV2(HttpServletRequest request, Model model) {
 		// 세션 관리자에 저장된 회원 정보 조회
 		Member member = (Member) sessionManager.getSession(request);
@@ -52,6 +53,23 @@ public class HomeController {
 		}
 		// 로그인
 		model.addAttribute("member", member);
+		return "loginHome";
+	}
+
+	@GetMapping("/")
+	public String homeLoginV3(HttpServletRequest request, Model model) {
+		// 세션이 없으면 home
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return "home";
+		}
+		Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+		// 세션에 회원 데이터가 없으면 home
+		if (loginMember == null) {
+			return "home";
+		}
+		// 세션이 유지되면 로그인으로 이동
+		model.addAttribute("member", loginMember);
 		return "loginHome";
 	}
 }
