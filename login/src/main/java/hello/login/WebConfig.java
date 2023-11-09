043f316ee06ea4f4e8fb894485af2eba42e9023f
LeeSPyo <oco141@naver.com>
2023-11-09 10:17:07 +0900

    스프링 인터셉터 - 인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -3,7 +3,6 @@ package hello.login;
 import javax.servlet.Filter;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -20,6 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addInterceptor(new LogInterceptor())
 			.order(1).addPathPatterns("/**")
 			.excludePathPatterns("/css/**", "/*.ico", "/error");
+		
+		registry.addInterceptor(new LoginCheckInterceptor())
+			.order(2)
+			.addPathPatterns("/**")
+			.excludePathPatterns(
+				"/", "/members/add", "/login", "/logout",
+				"/css/**", "/*.ico", "/error"
+			);
 	}
 
 	//@Bean
